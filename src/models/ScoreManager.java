f53238f40c620423143070ad52e3d2236582b466
Paul-Gomez <paulgomezzubi@gmail.com>
2025-01-03 19:05:04 +0100

    Por último creo los programas SocreManager.java y FileUtils.java

--- src/models/ScoreManager.java
+++ src/models/ScoreManager.java
@@ -1,4 +1,41 @@
 package models;
 
+import java.io.*;
+import java.util.*;
+
 public class ScoreManager {
+    private static final String SCORE_FILE = "resources/scores.dat";
+    private List<Player> highScores = new ArrayList<>();
+
+    public ScoreManager() {
+        loadScores();
+    }
+
+    public void loadScores() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(SCORE_FILE))) {
+            highScores = (List<Player>) ois.readObject();
+        } catch (Exception e) {
+            highScores = new ArrayList<>();
+        }
+    }
+
+    public void saveScores() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(SCORE_FILE))) {
+            oos.writeObject(highScores);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addScore(Player player) {
+        highScores.add(player);
+        highScores.sort(Comparator.comparingInt(Player::getScore).reversed());
+        if (highScores.size() > 5) {
+            highScores.remove(highScores.size() - 1);
+        }
+    }
+
+    public List<Player> getHighScores() {
+        return highScores;
+    }
 }
--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,4 +1,19 @@
 package utils;
 
+import java.io.*;
+import java.util.*;
+
 public class FileUtils {
+    public static List<String> readMoviesFromFile(String filePath) {
+        List<String> movies = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                movies.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
 }
