fa242ca9744725f50f11bf08a6dd7e27f9773592
Paul-Gomez <paulgomezzubi@gmail.com>
2025-01-03 19:02:38 +0100

    Sigo con los programas Movie.java y Player.java

--- src/models/Movie.java
+++ src/models/Movie.java
@@ -1,4 +1,25 @@
 package models;
 
 public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title.toLowerCase();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMaskedTitle(String guessedLetters) {
+        StringBuilder masked = new StringBuilder();
+        for (char c : title.toCharArray()) {
+            if (Character.isLetter(c)) {
+                masked.append(guessedLetters.contains(String.valueOf(c)) ? c : '*');
+            } else {
+                masked.append(c);
+            }
+        }
+        return masked.toString();
+    }
 }
--- src/models/Player.java
+++ src/models/Player.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Player {
+    private String nickname;
+    private int score;
+
+    public Player(String nickname, int score) {
+        this.nickname = nickname;
+        this.score = score;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
